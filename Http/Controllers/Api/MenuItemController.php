3d8c32b978468688325a7e9737eeedec669b5bde
TecnoDesign <marcos21.009@gmail.com>
2024-05-07 15:50:56 -0500

    Laravel 10 compatibility

--- Http/Controllers/Api/MenuItemController.php
+++ Http/Controllers/Api/MenuItemController.php
@@ -3,11 +3,16 @@
 namespace Modules\Menu\Http\Controllers\Api;
 
 use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Response;
+use Modules\Menu\Entities\Menuitem;
+use Modules\Menu\Http\Requests\CreateMenuItemRequest;
+use Modules\Menu\Http\Requests\UpdateMenuItemRequest;
 use Modules\Menu\Repositories\MenuItemRepository;
 use Modules\Menu\Services\MenuOrdener;
+use Modules\Menu\Transformers\MenuitemTransformer;
 
 class MenuItemController extends Controller
 {
@@ -29,6 +34,143 @@ class MenuItemController extends Controller
         $this->cache = $cache;
         $this->menuOrdener = $menuOrdener;
         $this->menuItem = $menuItem;
+    }
+
+    /**
+     * GET ITEMS
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try {
+
+            $params = $this->getParamsRequest($request);
+
+            $menuitems = $this->menuitem->getItemsBy($params);
+
+            $response = ['data' => MenuitemTransformer::collection($menuitems)];
+
+            $params->page ? $response["meta"] = ["page" => $this->pageTransformer($menuitems)] : false;
+
+        } catch (\Exception $e) {
+            \Log::error($e);
+            $status = $this->getStatusError($e->getCode());
+            $response = ["errors" => $e->getMessage()];
+
+        }
+
+        return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+    }
+
+    /**
+     * GET A ITEM
+     *
+     * @param string $criteria
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function show(Menuitem $menuitem, Request $request): JsonResponse
+    {
+        try {
+
+            $response = ["data" => new MenuitemTransformer($menuitem)];
+
+        } catch (\Exception $e) {
+
+            $status = $this->getStatusError($e->getCode());
+            $response = ["errors" => $e->getMessage()];
+        }
+
+        return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+    }
+
+    /**
+     * CREATE A ITEM
+     *
+     * @param CreateMenuItemRequest $request
+     * @return JsonResponse
+     */
+    public function create(CreateMenuItemRequest $request): JsonResponse
+    {
+        \DB::beginTransaction();
+        try {
+            $data = $request->all();
+
+            $menuItem = $this->menuitem->create($data);
+
+            $response = ["data" => new MenuitemTransformer($menuItem)];
+            \DB::commit();
+
+        } catch (\Exception $e) {
+
+            \DB::rollback();
+            $status = $this->getStatusError($e->getCode());
+            $response = ["errors" => $e->getMessage()];
+        }
+
+        return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+    }
+
+    /**
+     * UPDATE ITEM
+     *
+     * @param string $criteria
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateItem(Menuitem $menuitem, UpdateMenuItemRequest $request): JsonResponse
+    {
+        \DB::beginTransaction();
+
+        try {
+
+            $data = $request->all();
+
+            $this->menuitem->update($menuitem, $data);
+
+            $response = ["data" => trans('menu::messages.menuitem updated')];
+
+            \DB::commit();
+
+        } catch (\Exception $e) {
+
+            \DB::rollback();
+            $status = $this->getStatusError($e->getCode());
+            $response = ["errors" => $e->getMessage()];
+
+        }
+
+        return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+    }
+
+    /**
+     * DELETE A ITEM
+     *
+     * @param string $criteria
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function deleteItem(Menuitem $menuitem): JsonResponse
+    {
+        \DB::beginTransaction();
+        try {
+            $this->menuitem->destroy($menuitem);
+
+            $response = ["data" => "Item deleted"];
+
+            \DB::commit();
+
+        } catch (\Exception $e) {
+
+            \DB::rollback();
+            $status = $this->getStatusError($e->getCode());
+            $response = ["errors" => $e->getMessage()];
+
+        }
+
+        return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
     }
 
     /**
--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -4,6 +4,31 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 $router->group(['prefix' => '/menuitem', 'middleware' => 'api.token'], function (Router $router) {
+    $router->get('/', [
+        'as' => 'api.menu.menuitem.index',
+        'uses' => 'MenuItemController@index',
+        'middleware' => 'token-can:menu.menuitems.index',
+    ]);
+    $router->get('/{menuitem}', [
+        'as' => 'api.menu.menuitem.show',
+        'uses' => 'MenuItemController@show',
+        'middleware' => 'token-can:menu.menuitems.index',
+    ]);
+    $router->post('/', [
+        'as' => 'api.menu.menuitem.create',
+        'uses' => 'MenuItemController@create',
+        'middleware' => 'token-can:menu.menuitems.create',
+    ]);
+    $router->put('{menuitem}', [
+        'as' => 'api.menuitem.updateItem',
+        'uses' => 'MenuItemController@updateItem',
+        'middleware' => 'token-can:menu.menuitems.edit',
+    ]);
+    $router->post('{menuitem}', [
+        'as' => 'api.menuitem.deleteItem',
+        'uses' => 'MenuItemController@deleteItem',
+        'middleware' => 'token-can:menu.menuitems.destroy',
+    ]);
     $router->post('/update', [
         'as' => 'api.menuitem.update',
         'uses' => 'MenuItemController@update',
@@ -15,17 +40,32 @@ $router->group(['prefix' => '/menuitem', 'middleware' => 'api.token'], function 
         'middleware' => 'token-can:menu.menuitems.destroy',
     ]);
 });
-$router->group(['prefix' => 'menu'], function (Router $router) {
-
-  // menu Routes
-  require('ApiRoutes/menuRoutes.php');
-
-  // menuItems Routes
-  require('ApiRoutes/menuItemRoutes.php');
-
-  // Legacy Api Routes
-  require ('ApiRoutes/menuLegacyApiRoutes.php');
-
+$router->group(['prefix' => 'menu', 'middleware' => 'api.token'], function (Router $router) {
+    $router->get('/', [
+        'as' => 'api.menu.menu.index',
+        'uses' => 'MenuApiController@index',
+        'middleware' => 'can:menu.menus.index',
+    ]);
+    $router->get('/{menu}', [
+        'as' => 'api.menu.menu.show',
+        'uses' => 'MenuApiController@show',
+        'middleware' => 'can:menu.menus.index',
+    ]);
+    $router->post('/', [
+        'as' => 'api.menu.menu.create',
+        'uses' => 'MenuApiController@create',
+        'middleware' => 'can:menu.menus.create',
+    ]);
+    $router->put('/{menu}', [
+        'as' => 'api.menu.menu.update',
+        'uses' => 'MenuApiController@update',
+        'middleware' => 'can:menu.menus.edit',
+    ]);
+    $router->delete('/{menu}', [
+        'as' => 'api.menu.menu.delete',
+        'uses' => 'MenuApiController@delete',
+        'middleware' => 'can:menu.menus.delete',
+    ]);
 });
 
 
